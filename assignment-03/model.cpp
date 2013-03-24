bc9ab691eb9cad9045fe03d47192c5f8e7436976
Michael Liu <m27liu@engmail.uwaterloo.ca>
2013-03-24 02:10:57 -0400

    additional improvements - precomputing length() and lengthSquared() values

--- assignment-03/model.cpp
+++ assignment-03/model.cpp
@@ -1,10 +1,10 @@
 #include "model.h"
 
 Model::Model() {
-    for (unsigned int i = 0; i < NUM_IMAGES; ++i) imgs[i] = new QImage();
+	for (unsigned int i = 0; i < NUM_IMAGES; ++i) imgs[i] = new QImage();
 
-    listLines = new vector<pair <QPoint, QPoint> >* [2];
-    listAux = new vector<pair <QPoint, QPoint> >* [2];
+	listLines = new vector<pair <QPoint, QPoint> >* [2];
+	listAux = new vector<pair <QPoint, QPoint> >* [2];
 }
 
 void Model::prepStraightLine() {
@@ -72,22 +72,22 @@ void Model::commonPrep() {
         y4 = listAux[0]->at(k).second.y();
 
         double m = (y4 - y3) / (x4 - x3);
-        // if x's are further apart than 'y's
+		// if x's are further apart than 'y's
         if(fabs(x4-x3) > fabs(y4-y3)) {
-            // swap coordinates if backwards
+			// swap coordinates if backwards
             if(x3 > x4) { swap(x3, x4); swap(y3, y4); }
 
-            // draw line, with the appropriate slope
+			// draw line, with the appropriate slope
             while(x3 < x4) {
                 y3 = m*(x3 - x4) + y4;
                 imgs[4]->setPixel(x3, y3, qRgb(0, 0, 0));
                 x3 += 0.01;
             }
         } else {
-            // swap coordinates if backwards
+			// swap coordinates if backwards
             if(y3 > y4) { swap(x3, x4); swap(y3, y4); }
 
-            // draw line, with the appropriate slope
+			// draw line, with the appropriate slope
             while(y3 < y4) {
                 x3 = (y3 - y4)/m + x4;
                 imgs[4]->setPixel(x3, y3, qRgb(0, 0, 0));
@@ -98,34 +98,56 @@ void Model::commonPrep() {
 }
 
 void Model::morph(int h, double VARA, double VARB, double VARP) {
-    int n = 0;
-    int lines = listLines[0]->size();
+	int n = 0;
+	int lines = listLines[0]->size();
+
+    qreal QPLengthSquared[lines];
+    qreal QPLength[lines];
+    qreal Q2P2Length[lines];
+
+    #pragma omp parallel
+    for(int k=0; k<lines; ++k) {
+		// get original lines from reference line
+		QPoint P = listLines[h]->at(k).first;
+		QPoint Q = listLines[h]->at(k).second;
+
+        QVector2D QP(Q - P);
+        QPLength[k] = QP.length();
+        QPLengthSquared[k] = QP.lengthSquared();
+
+        // get interpolating lines from reference line
+        QPoint P2 = listAux[h]->at(k).first;
+        QPoint Q2 = listAux[h]->at(k).second;
+
+        QVector2D Q2P2(Q2 - P2);
+        Q2P2Length[k] = Q2P2.length();
+    }
 
     #pragma omp parallel for collapse (2)
-    for(int i=0; i<wimg; ++i) {
-        for(int j=0; j<himg; ++j) {
-            
-            QPoint X(i, j);
-            double u, v;
-            
+	for(int i=0; i<wimg; ++i) {
+		for(int j=0; j<himg; ++j) {
+			
+			QPoint X(i, j);
+			double u, v;
+			
             QPoint sum(0.0, 0.0);
             double wsum = 0;
- 
-            // for each line
-            for(int k=0; k<lines; ++k) {
-                
-                // get original lines from reference line
-                QPoint P = listLines[h]->at(k).first;
-                QPoint Q = listLines[h]->at(k).second;
-                
-                QVector2D XP(X - P);
-                QVector2D QP(Q - P);
-                
-                QVector2D pQP(QP.y(), -QP.x());
-                
-                // Calculate u, v
-                u = QVector2D::dotProduct(XP, QP) /  QP.lengthSquared();
-                v = QVector2D::dotProduct(XP, pQP) / QP.length();
+			
+			// for each line
+			for(int k=0; k<lines; ++k) {
+				
+				// get original lines from reference line
+				QPoint P = listLines[h]->at(k).first;
+				QPoint Q = listLines[h]->at(k).second;
+				
+				QVector2D XP(X - P);
+				QVector2D QP(Q - P);
+				
+				QVector2D pQP(QP.y(), -QP.x());
+				
+				// Calculate u, v
+				u = QVector2D::dotProduct(XP, QP) /  QPLengthSquared[k];
+				v = QVector2D::dotProduct(XP, pQP) / QPLength[k];
 
                 // get interpolating lines from reference line
                 QPoint P2 = listAux[h]->at(k).first;
@@ -134,7 +156,7 @@ void Model::morph(int h, double VARA, double VARB, double VARP) {
                 QVector2D Q2P2(Q2 - P2);
                 QVector2D pQ2P2(Q2P2.y(), -Q2P2.x());
 
-                QVector2D X2 = QVector2D(P2) + u * Q2P2 + (v * pQ2P2) / Q2P2.length();
+                QVector2D X2 = QVector2D(P2) + u * Q2P2 + (v * pQ2P2) / Q2P2Length[k];
 
                 QPoint p = X2.toPoint() - X;
 
@@ -144,15 +166,16 @@ void Model::morph(int h, double VARA, double VARB, double VARP) {
                 else dist = sqrt(pow(X.x() - Q.x(), 2.0) + pow(X.y() - Q.y(), 2.0));
 
                 double w = 0;
-                w =  pow(QP.length(), VARP);
+                w =  pow(QPLength[k], VARP);
                 w /= (VARA + dist);
                 w = pow(w, VARB);
 
-                sum += w * p;
+                sum  += w * p;
                 wsum += w;
             }
 
             sum /= wsum;
+
             QPoint X2 = X + sum;
 
             double y0, x0;
